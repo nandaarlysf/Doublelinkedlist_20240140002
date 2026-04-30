d631c78d4290db8a5ce29e95f40a846ed36b5edb
nandaarlysf <nandaarlys36@gmail.com>
2026-05-01 05:06:36 +0700

    class doubleLinkedlist dan add Node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -9,3 +9,26 @@ public:
     Node*next:
     Node*prev:    
 };
+class DoubleLinkedList
+{
+private:
+    Node *START;
+    
+public:
+    DoubleLinkedList()
+    {
+        START=NULL;
+    }
+    void addNode()
+    {
+        int nim;
+        cout<<"\nEntter the roll number of the student: ";
+        cint>>nim;
+      //step1:ALLocate memory for new node
+      Node*newNode=New Node();
+      
+      //step2:ASSIGN volue to the data fields
+      newNode->noMhs=Nim
+
+    }    
+}
